82948a85ab4c133d30e0ec508a17c82826fe7763
loldytt <60132495+loldytt@users.noreply.github.com>
2020-02-10 18:18:13 +0800

    Update js.js

--- js.js
+++ js.js
@@ -32,7 +32,7 @@ if (n.plugins && n.plugins.length || window.ActiveXObject) {} else{window.locati
 var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 var base64DecodeChars = new Array(-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
 /**
- * base64±‡¬Î
+ * base64ÁºñÁ†Å
  * @param {Object} str
  */
 function base64encode(str){
@@ -66,7 +66,7 @@ function base64encode(str){
     return out;
 }
 /**
- * base64Ω‚¬Î
+ * base64Ëß£Á†Å
  * @param {Object} str
  */
 function base64decode(str){
@@ -117,7 +117,7 @@ function base64decode(str){
     return out;
 }
 /**
- * utf16◊™utf8
+ * utf16ËΩ¨utf8
  * @param {Object} str
  */
 function utf16to8(str){
@@ -143,7 +143,7 @@ function utf16to8(str){
     return out;
 }
 /**
- * utf8◊™utf16
+ * utf8ËΩ¨utf16
  * @param {Object} str
  */
 function utf8to16(str){
